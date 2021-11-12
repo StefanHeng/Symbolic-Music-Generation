35f5c03fac288d64970afc4c6db43228c5dd6136
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2021-11-12 12:15:49 -0500

    Update: midi processing, tempo change; Chore: util refractor

--- util.py
+++ util.py
@@ -4,8 +4,14 @@ import glob
 from functools import reduce
 
 import pandas as pd
+from mido import MidiFile
 
 from data_path import *
+
+
+def flatten(lsts):
+    """ Flatten list of list of elements to list of elements """
+    return [e for lst in lsts for e in lst]
 
 
 def read_pickle(fnm):
@@ -66,13 +72,46 @@ def get_midi_paths(dnm):
     return paths
 
 
-def plot_single_instrument(instr_, cols=['start', 'end', 'pitch', 'velocity'], n=None):
-    """
-    Plots a single `pretty_midi.Instrument` channel
-    """
-    def _get_get(note):
-        return [getattr(note, attr) for attr in cols]
-    # df = pd.DataFrame([[n.start, n.end, n.pitch, n.velocity] for n in instr_.notes], columns=cols)
-    df = pd.DataFrame([_get_get(n) for n in instr_.notes], columns=cols)[:n]
-    df.plot(figsize=(16, 9), lw=0.25, ms=0.3, title=f'Instrument notes plot - [{",".join(cols)}]')
-    return df
+MIDO_TPO = int(5e5)  # Midi default tempo (ms per tick, i.e. 120 BPM)
+
+
+class MidoUtil:
+    @staticmethod
+    def get_msgs_by_type(midi, t_):
+        def _get(track):
+            return list(filter(lambda tr: tr.type == t_, track))
+        if type(midi) is MidiFile:
+            return {i: _get(trk) for i, trk in enumerate(midi.tracks)}
+        else:  # midi.MidiTrack
+            return _get(midi)
+
+    @staticmethod
+    def get_tempo_changes(midi):
+        lst_msgs = MidoUtil.get_msgs_by_type(midi, 'set_tempo')
+        # if msgs:
+        #     return
+        if lst_msgs:
+            lst_msgs = list(filter(bool, lst_msgs.values()))
+            lst_tempo = list(map(lambda lst: list(map(lambda msg: msg.tempo, lst)), lst_msgs))
+            return flatten(lst_tempo)
+        # ic(lst)
+        else:
+            return [MIDO_TPO]
+
+
+class PrettyMidiUtil:
+    INSTR_ATTRS = ('start', 'end', 'pitch', 'velocity')
+
+    @staticmethod
+    def plot_single_instrument(instr_, cols=INSTR_ATTRS, n=None):
+        """
+        Plots a single `pretty_midi.Instrument` channel
+
+        :return: pd.DataFrame of respective attributes in the note, up until `n`
+        """
+        def _get_get(note):
+            return [getattr(note, attr) for attr in cols]
+        # df = pd.DataFrame([[n.start, n.end, n.pitch, n.velocity] for n in instr_.notes], columns=cols)
+        df = pd.DataFrame([_get_get(n) for n in instr_.notes], columns=cols)[:n]
+        df.plot(figsize=(16, 9), lw=0.25, ms=0.3, title=f'Instrument notes plot - [{",".join(cols)}]')
+        return df
