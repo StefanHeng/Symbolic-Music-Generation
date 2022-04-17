9b1b5ed8b3d465d314af02ebf4a98523faacdf1e
xysong <xysong25@gmail.com>
2022-04-17 16:01:34 -0400

    Update: metrics.py get_off_key_ratio() fix

--- musicnlp/models/metrics.py
+++ musicnlp/models/metrics.py
@@ -4,16 +4,19 @@ from collections import Counter
 
 import numpy as np
 import music21
+from responses import target
 
 from musicnlp.util import *
 from musicnlp.util.train import PT_LOSS_PAD
 from musicnlp.vocab import VocabType, MusicTokenizer
+from musicnlp.vocab.elm_type import *
 
 
 class IkrMetric:
     """
     Vectorized metric of matched keys per pitch, based on `_get_off_key_ratio`
     """
+
     def __init__(self, tokenizer: MusicTokenizer, n_init_bars: int = 4):
         self.tokenizer = tokenizer
         self.vocab = tokenizer.vocab
@@ -32,54 +35,82 @@ class IkrMetric:
         return 1 - np.mean([self.get_off_key_ratio(gen, gt)for gen, gt in zip(gens, gts)])
 
     def get_init_key_est(self, gt_token_seq: Union[str, List[str]]):
-        tok_lst = gt_token_seq.split() if isinstance(gt_token_seq, str) else gt_token_seq
+        tok_lst = gt_token_seq.split() if isinstance(
+            gt_token_seq, str) else gt_token_seq
 
         # Heuristics to determine starting bar
-        bar_idx = [idx for idx, tok in enumerate(tok_lst) if tok == self.vocab.start_of_bar]
+        bar_idx = [idx for idx, tok in enumerate(
+            tok_lst) if tok == self.vocab.start_of_bar]
         assert len(bar_idx) > self.n_init_bars + 1, \
             f'Not enough bars for key estimation: expect at least {logi(self.n_init_bars + 1)} total bars in music, ' \
             f'got {logi(len(bar_idx))}'
 
         pitch_lst = list(
-            filterfalse(lambda x: self.vocab.type(x) != VocabType.pitch, tok_lst[:bar_idx[self.n_init_bars]])
+            filterfalse(lambda x: self.vocab.type(x) !=
+                        VocabType.pitch, tok_lst[:bar_idx[self.n_init_bars]])
         )
-        key_cls = [music21.pitch.Pitch(midi=self.vocab.compact(p)).pitchClass for p in pitch_lst]
+        key_cls = [music21.pitch.Pitch(
+            midi=self.vocab.compact(p)).pitchClass for p in pitch_lst]
         key_est = Counter(key_cls).most_common()[0][0]
         return key_est
 
-    def get_off_key_ratio(
-            self, gen_token_text: Union[str, List[str]], gt_token_text: Union[str, List[str]]
-    ) -> float:
+    def get_pred_stats_pitch(self, preds: List[int]):
         """
-        For a single song
+        Helper function for sanity check.
+        Given a single song, print out relevant statistics of its pitches.
+        Args:
+            preds (List[int]): List of tokens (represented in ints)
+        """
+        tok_lst = preds.split() if isinstance(preds, str) else preds
+        pitch_lst = list(filterfalse(
+            lambda x: self.vocab.type(x) != VocabType.pitch, tok_lst))
+        stats_pitch_cls_int = Counter([music21.pitch.Pitch(
+            midi=self.vocab.compact(p)).pitchClass for p in pitch_lst])
+        stats_pitch_cls_str = Counter([music21.pitch.Pitch(
+            midi=self.vocab.compact(p)).name for p in pitch_lst])
+        ic(stats_pitch_cls_str)
+        ic(stats_pitch_cls_int)
 
-        Both arguments are either jointed string of tokens or split list of tokens
-        """
-        tok_lst = gen_token_text.split() if isinstance(gen_token_text, str) else gen_token_text
-        target_key = self.get_init_key_est(gt_token_text)
-        pitch_lst = list(filterfalse(lambda x: self.vocab.type(x) != VocabType.pitch, tok_lst))
+    def get_off_key_ratio(
+            self, preds: List[int], key: Key
+    ) -> float:
+
+        tok_lst = preds.split() if isinstance(preds, str) else preds
+        target_key = key
+        pitch_lst = list(filterfalse(
+            lambda x: self.vocab.type(x) != VocabType.pitch, tok_lst))
         num_toks = len(pitch_lst)
-        if num_toks == 0:  # No pitch found, assume every pitch is off-note
+        # No pitch found, assume every pitch is off-note
+        if num_toks == 0:
             return 1
-        # Heuristics
-        # TODO: add more music theories later
-        key_thres, key_count, num_off_key = 8, 0, 0
-        prev_p_cls = None
-        num_off_key = 0
-        for p in pitch_lst:
-            p_cls = music21.pitch.Pitch(midi=self.vocab.compact(p)).pitchClass
-            if p_cls != target_key:
-                num_off_key += 1
-                # TODO: add cross-bar detection
-                if prev_p_cls is not None and p_cls == prev_p_cls:
-                    key_count += 1
-                    if key_count >= key_thres:
-                        target_key = p_cls
-                        key_count = 0
-                prev_p_cls = p_cls
-            else:
-                prev_p_cls = None
-        return num_off_key / num_toks
+        # Process the given key
+        key_type, key_name = key_enum2tuple[target_key]
+        # Extract midi values for all available pitches
+        pitch_midi = np.array([self.vocab.compact(p) for p in pitch_lst])
+        key_offset = key_offset_dict[key_name]
+        pred_offset = ((pitch_midi % 12) - key_offset) % 12
+        in_key_lst = list(filterfalse(
+            lambda x: x in OFFKEY_OFFSET[key_type], pred_offset))
+        in_key_ratio = len(in_key_lst) / num_toks
+        # # Heuristics
+        # # TODO: add more music theories later
+        # key_thres, key_count, num_off_key = 8, 0, 0
+        # prev_p_cls = None
+        # num_off_key = 0
+        # for p in pitch_lst:
+        #     p_cls = music21.pitch.Pitch(midi=self.vocab.compact(p)).pitchClass
+        #     if p_cls != target_key:
+        #         num_off_key += 1
+        #         # TODO: add cross-bar detection
+        #         if prev_p_cls is not None and p_cls == prev_p_cls:
+        #             key_count += 1
+        #             if key_count >= key_thres:
+        #                 target_key = p_cls
+        #                 key_count = 0
+        #         prev_p_cls = p_cls
+        #     else:
+        #         prev_p_cls = None
+        return in_key_ratio
 
 
 if __name__ == '__main__':
@@ -93,6 +124,7 @@ if __name__ == '__main__':
     from musicnlp.preprocess import MusicExtractor, KeyFinder
 
     song_nm = 'Merry Go Round of Life'
+    # song_nm = '平凡之路'
 
     def write_eg_song_json(song_name: str = 'Merry Go Round of Life'):
         fnm = music_util.get_my_example_songs(song_name, fmt='MXL')
@@ -102,25 +134,27 @@ if __name__ == '__main__':
         score = me(fnm, exp=exp)
         with open(os.path.join(music_util.get_processed_path(), f'{song_name}.json'), 'w') as f:
             json.dump(dict(score=score), f, indent=2)
-    write_eg_song_json(song_nm)
+    # write_eg_song_json(song_nm)
 
     def get_eg_song_key(song_name: str = 'Merry Go Round of Life'):
         fnm = music_util.get_my_example_songs(song_name, fmt='MXL')
 
         kf = KeyFinder(fnm)
-        keys = kf.find_key()
+        keys = kf.find_key(return_type="enum")
         ic(keys)
         # ic(kf.find_scale_degrees(keys))
-    # get_eg_song_key(song_nm)
+    get_eg_song_key(song_nm)
 
     im = IkrMetric(MusicTokenizer(), n_init_bars=2)
 
     def check_key_metric():
-        # text = music_util.get_extracted_song_eg(k='平凡之路')  # this one has tuplets
+        # text = music_util.get_extracted_song_eg(
+        # k='平凡之路')  # this one has tuplets
         with open(os.path.join(music_util.get_processed_path(), f'{song_nm}.json'), 'r') as f:
             text = json.load(f)['score']
-        ic(text[:200])
-        ic(im.get_off_key_ratio(text, text))
+        # ic(text[:200])
+        # ic(im.get_off_key_ratio(text, Key.AfMaj))
+        ic(im.get_off_key_ratio(text, Key.DMin))
     check_key_metric()
 
     def check_init_key_no_error():
@@ -139,7 +173,8 @@ if __name__ == '__main__':
         tokenizer = MusicTokenizer(prec=5)
         tokenizer.model_max_length = 2048  # TODO: hard-code for now
         dset = get_dataset(
-            dataset_names=dnms, map_func=lambda x: tokenizer(x['score'], padding='max_length', truncation=True),
+            dataset_names=dnms, map_func=lambda x: tokenizer(
+                x['score'], padding='max_length', truncation=True),
             remove_columns=['title', 'score', 'duration'], n_sample=n_sample, shuffle_seed=seed
         )
         # effectively get the fist tokens of model size, simulating training data-loading
@@ -150,9 +185,11 @@ if __name__ == '__main__':
                 # text = tokenizer.decode(d['input_ids'])
                 # ic(text)
                 # im.get_init_key_est(text)
-                ids = np.array(d['input_ids']).reshape(1, -1)  # dummy batch dim
+                ids = np.array(d['input_ids']).reshape(
+                    1, -1)  # dummy batch dim
                 # ic(ids.shape)
-                im(ids, ids)  # effectively we're only checking the ground-truth init key part
+                # effectively we're only checking the ground-truth init key part
+                im(ids, ids)
                 # exit(1)
     # check_init_key_no_error()
     # profile_runtime(check_init_key_no_error)
--- musicnlp/vocab/elm_type.py
+++ musicnlp/vocab/elm_type.py
@@ -14,7 +14,8 @@ class Key(Enum):
     See musicnlp.preprocess.key_finder.py
     """
     CMaj, FMaj, BfMaj, EfMaj, AfMaj, DfMaj, GfMaj, BMaj, EMaj, AMaj, DMaj, GMaj, \
-        AMin, DMin, GMin, CMin, FMin, BfMin, EfMin, GsMin, CsMin, FsMin, BMin, EMin = list(range(24))
+        AMin, DMin, GMin, CMin, FMin, BfMin, EfMin, GsMin, CsMin, FsMin, BMin, EMin = list(
+            range(24))
 
     @classmethod
     def from_str(cls, key: str) -> 'Key':
@@ -48,6 +49,65 @@ key_str2enum: Dict[str, Key] = {
     'EMinor': Key.EMin,
 }
 
+# Dictionary for translating a Key type to a tuple of ints (type, key)
+# where type := major (1) or minor (0) and key := the name of the keyC
+key_enum2tuple: Dict[Key, tuple[int, str]] = {
+    Key.CMin: (0, 'C'),
+    Key.CsMin: (0, 'C#'),
+    Key.DMin: (0, 'D'),
+    Key.EfMin: (0, 'E-'),
+    Key.EMin: (0, 'E-'),
+    Key.FMin: (0, 'F'),
+    Key.FsMin: (0, 'F#'),
+    Key.GMin: (0, 'G'),
+    Key.GsMin: (0, 'G#'),
+    Key.AMin: (0, 'A'),
+    Key.BfMin: (0, 'B-'),
+    Key.BMin: (0, 'B'),
+    Key.CMaj: (1, 'C'),
+    Key.DMaj: (1, 'D'),
+    Key.DfMaj: (1, 'D-'),
+    Key.EfMaj: (1, 'E-'),
+    Key.EMaj: (1, 'E'),
+    Key.FMaj: (1, 'F'),
+    Key.GMaj: (1, 'G'),
+    Key.GfMaj: (1, 'G-'),
+    Key.AMaj: (1, 'A'),
+    Key.AfMaj: (1, 'A-'),
+    Key.BfMaj: (1, 'B-'),
+    Key.BMaj: (1, 'B')
+}
+
+# This does not take ENHARMONICS into account
+# TODO: Fix this by adding ENHARMONIC relations
+key_offset_dict: Dict[str, int] = {
+    'C': 0,
+    'C#': 1,
+    'D-': 1,
+    'D': 2,
+    'D#': 3,
+    'E-': 3,
+    'E': 4,
+    'F': 5,
+    'F#': 6,
+    'G-': 6,
+    'G': 7,
+    'G#': 8,
+    'A-': 8,
+    'A': 9,
+    'B-': 10,
+    'B': 11
+}
+MAJOR_OFFKEY_OFFSET_IDX = [1, 3, 6, 8, 10]
+# Harmonic key (INKEY) offset for MINOR: [0, 2, 3, 5, 7, 8, (10 or 11)]
+MINOR_OFFKEY_OFFSET_IDX = [1, 4, 6, 9, 11]
+OFFKEY_OFFSET = [MINOR_OFFKEY_OFFSET_IDX, MAJOR_OFFKEY_OFFSET_IDX]
 
 # an intermediate representation, for conversion between music string & MXL
-MusicElement = namedtuple(typename='MusicElement', field_names=['type', 'meta'], defaults=[None, None])
+MusicElement = namedtuple(typename='MusicElement', field_names=[
+                          'type', 'meta'], defaults=[None, None])
+
+
+if __name__ == '__main__':
+    assert len(key_str2enum) == len(
+        key_enum2str), "Dictionary should be of the same size"
