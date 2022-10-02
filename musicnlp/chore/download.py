d958b071391421f7994ade95e54a1d388ced3864
Stefan Heng <stefan.hg@outlook.com>
2022-10-02 19:55:55 +0800

    Update: try down songs

--- musicnlp/chore/download.py
+++ musicnlp/chore/download.py
@@ -15,10 +15,20 @@ import musicnlp.util.music as music_util
 
 logger = get_logger('Download')
 
+
+DROPBOX_LMD_FNM = '22-05-27_LMD, all.zip'
+
 CONVERTED_FILES2URL = {
     # (`conversion backend`, `dataset name`) => Google Drive link url
     # a folder containing converted files from both MuseScore & Logic Pro
-    ('All', 'LMD'): 'https://drive.google.com/uc?id=1CyfKiVX83YdS4p7_4npk2xbDVJ68L0tg',
+    # this one from UMich GDrive, some problem w/ gdown and large files
+    # ('All', 'LMD'): 'https://drive.google.com/uc?id=1CyfKiVX83YdS4p7_4npk2xbDVJ68L0tg',
+    ('MuseScore', 'LMD'): [
+        'https://drive.google.com/uc?id=1-ISc2u6Sxvs3LES4byx0KcNGGVYDZnxV',
+        'https://drive.google.com/uc?id=1LdXavGJnCeoKn2ZnkbbpYTbdwpNTjrLe',
+        'https://drive.google.com/uc?id=11ci6rw6NqdoNEDgKIcTN_XZxRgG-Ie3K'
+    ],
+    ('Logic Pro', 'LMD'): 'https://drive.google.com/uc?id=1ZAxpnu9md7FY-5Cq9I8deAXWbOY0MZBs',
     ('MuseScore', 'MAESTRO'): 'https://drive.google.com/uc?id=1fzmfS65BN84O_bF1v8dN2uFlrrpOzYaZ',
     ('MuseScore', 'POP909'): 'https://drive.google.com/uc?id=1XobTD6x88PIEKfrZ6IAzXjMaZmBZ0XqR'
 }
@@ -36,30 +46,57 @@ HF_DSETS2URL = {
 TOKENIZER_URL = 'https://drive.google.com/uc?id=1rbQccozpAMjRWkjtKConka_DkCusxZsF'
 
 
-def download_n_unzip(url: str = None, download_output_path: str = None):
+def download_n_unzip(url: str = None, download_output_path: str = None, extract_path: str = None):
     if not os.path.exists(download_output_path):
         logger.info(f'Downloading file from {logi(url)} to {logi(download_output_path)}... ')
         gdown.download(url, download_output_path, quiet=False)
 
-    logger.info(f'Unzipping downloaded file {logi(download_output_path)}... ')
+    ext_pa = extract_path or os.path.dirname(download_output_path)
+    logger.info(f'Unzipping downloaded file {logi(download_output_path)} to {logi(ext_pa)}... ')
     with ZipFile(download_output_path, 'r') as zf:
-        zf.extractall(os.path.dirname(download_output_path))
+        zf.extractall(ext_pa)
 
 
 if __name__ == '__main__':
-    conv_base = os_join(get_output_base(), u.dset_dir, 'converted')
+    conv_base = os_join(get_output_base(), u.dset_dir, 'converted-debug')
     hf_base = os_join(music_util.get_processed_path(), 'hf')
     mic(conv_base, hf_base)
     os.makedirs(conv_base, exist_ok=True)
     os.makedirs(hf_base, exist_ok=True)
 
+    def down_from_dropbox():
+        # TODO: no longer needed since no `dropbox` python package in linux
+        import dropbox
+        import json
+        with open(os_join(u.proj_path, 'auth', 'dropbox-access-token.json')) as f:
+            token = json.load(f)['access-token']
+        dbx = dropbox.Dropbox(token)
+        # for entry in dbx.files_list_folder('').entries:
+        #     mic(entry.name, entry)
+        mic(dbx)
+        with open(os_join(conv_base, DROPBOX_LMD_FNM), 'wb') as f:
+            metadata, result = dbx.files_download(path=f'/{DROPBOX_LMD_FNM}')
+            mic(metadata, type(result))
+            f.write(result.content)
+    # down_from_dropbox()
+
     def down_converted_single(back_end: str = None, dataset_name: str = None):
         fnm = f'Converted_{{be={back_end}, dnm={dataset_name}}}'
-        out_path = os_join(conv_base, f'{fnm}.zip')
-        download_n_unzip(url=CONVERTED_FILES2URL[(back_end, dataset_name)], download_output_path=out_path)
+        url = CONVERTED_FILES2URL[(back_end, dataset_name)]
+        if isinstance(url, list):
+            assert back_end == 'MuseScore' and dataset_name == 'LMD'
+            dnm = 'LMD, MS'
+            for i, url_ in enumerate(url, start=1):
+                out_path = os_join(conv_base, f'{fnm}_split {i}.zip')
+                ext_path = os_join(conv_base, dnm)
+                download_n_unzip(url=url_, download_output_path=out_path, extract_path=ext_path)
+        else:
+            out_path = os_join(conv_base, f'{fnm}.zip')
+            download_n_unzip(url, out_path)
     # down_converted_single(back_end='MuseScore', dataset_name='POP909')
     # down_converted_single(back_end='MuseScore', dataset_name='MAESTRO')
-    # down_converted_single(back_end='All', dataset_name='LMD')
+    down_converted_single(back_end='MuseScore', dataset_name='LMD')
+    down_converted_single(back_end='Logic Pro', dataset_name='LMD')
 
     def move_converted_lmd():
         """
